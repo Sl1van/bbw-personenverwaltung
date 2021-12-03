86d54c8fd6839706d2070f17a4abd96d51e3803f
Sl1van <>
2021-12-03 09:37:50 +0100

    added basic functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,22 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        {getPeople().map(person => {
+          return <tr><td>{person.firstName}</td><td>{person.lastName}</td></tr>
+        })}
+      </table>
     </div>
   );
+}
+
+function getPeople() {
+  return [{id: 1,firstName: "Chad",lastName: "Champion"}];
 }
 
 export default App;
