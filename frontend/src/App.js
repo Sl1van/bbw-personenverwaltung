7edd712d969c2fa6344231a27402a07db36ec53c
YassinW <yassin.wafa@gmail.com>
2021-12-10 11:05:41 +0100

    add a lot of better code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,86 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState } from 'react';
-
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  const [people, setPeople] = useState(0);
-  
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    fetchPeople(setPeople);
+  }, []);
+
+  function fetchPeople(setter) {
+    axios
+      .get("http://localhost:8080/person")
+      .then((result) => setter(result.data));
+    // fetch("http://localhost:8080/person", {
+    //   method: "GET",
+    //   mode: "no-cors",
+    // }).then(res => setter(res.data))
+    // }).then(result => setter([{ id: 1, firstName: "Chad", lastName: "Champion" }]))
+  }
+
+  function deletePerson(id) {
+    axios.delete("http://localhost:8080/person/" + id).then(() => {
+      window.location.reload();
+    });
+  }
+
+  function createPerson(firstName, lastName, email) {
+    axios
+      .post(
+        "http://localhost:8080/person",
+        JSON.stringify({
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+        }),
+        { headers: { "Content-Type": "application/json" } }
+      )
+      .then((response) => console.log(response))
+      .then(() => {
+        window.location.reload();
+      });
+  }
+
+  function editPerson(id, firstName, lastName, email) {
+    axios
+      .patch(
+        "http://localhost:8080/person",
+        { id: id },
+        JSON.stringify({
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+        }),
+        { headers: { "Content-Type": "application/json" } }
+      )
+      .then((response) => console.log(response));
+  }
+
   return (
     <div className="App">
       <table>
-        {getPeople().map(person => {
-          return 
-          <tr>
-            <td>{person.firstName}</td>
-            <td>{person.lastName}</td>
-            <td>
-              <button onClick={deletePerson(person.id)}>Delete</button>
-            </td>
-          </tr>;
-        })}
+        <tr>
+          <td>Vorname</td>
+          <td>Nachname</td>
+          <td>Email</td>
+          <td></td>
+        </tr>
+        {people &&
+          people.map((person) => (
+            <tr>
+              <td>{person.firstName}</td>
+              <td>{person.lastName}</td>
+              <td>{person.email}</td>
+              <td>
+                <button
+                  onClick={() => {
+                    deletePerson(person.id);
+                  }}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
       </table>
-      <button onClick={() =>{
-        //console.log("Test")
-        createPerson("New", "User", "new@user.com");
-        //setPeople(getPeople())
-      }}>
+      <button
+        onClick={() => {
+          createPerson("New", "User", "new@user.com");
+        }}
+      >
         add User
-        </button>
+      </button>
     </div>
   );
-}
-
-function getPeople() {
-  fetch('http://localhost:8080/person', {
-    method: 'GET',
-    mode: 'no-cors'
-  })
-  return [{id: 1,firstName: "Chad",lastName: "Champion"}];
-}
-
-function deletePerson(id) {
-  fetch('http://localhost:8080/person' + id, 
-  {
-    method: 'DELETE',
-    mode: 'no-cors'
-  })
-}
-
-function createPerson(firstName, lastName, email) {
-  console.log(JSON.stringify({
-    firstName: firstName,
-    lastName: lastName,
-    email: email
-  }));
-  fetch('http://localhost:8080/person', {
-    method: 'POST',
-    mode: 'no-cors',
-    body: JSON.stringify({
-      firstName: firstName,
-      lastName: lastName,
-      email: email
-    }),
-    headers: {
-      Accept: 'application/json'
-    }
-  })
-}
-
-function editPerson(id, firstName, lastName, email) {
-  fetch('http://localhost:8080/person', {
-    method: 'PATCH',
-    mode: 'no-cors',
-    body: JSON.stringify({
-      id: id,
-      firstName: firstName,
-      lastName: lastName,
-      email: email
-    }),
-    headers: {
-      Accept: 'application/json'
-    }
-  })
 }
 
 export default App;
